aedbc95dc80e5bf529ae9a01e7ff2e6f85b84fd6
pyuxiang <pehyuxiang@gmail.com>
2026-05-25 15:36:51 +0800

    fix: double termination bug in TDC1 after timestamp mode

--- src/inst_efficiency/tdc1_utils.py
+++ src/inst_efficiency/tdc1_utils.py
@@ -1,6 +1,7 @@
-import time
 import pathlib
 import sys
+import time
+import types
 
 import numpy as np
 import S15lib.g2lib.g2lib as g2
@@ -29,8 +30,26 @@ def load(args):
     timestamp.threshold = args.threshvolt
     timestamp.accumulated_timestamps_filename = args.tmpfile
     timestamp._com.write(b"abort\r\n")  # terminate any existing streams
-    while timestamp._com.in_waiting:
-        timestamp._com.readlines()  # empties buffer
+    timestamp._com.readlines()  # drain buffer
+
+    # 2026-05-25, on TDC1-0049, there is a bug that occurs after TDC1 enters timestamp mode,
+    # and requires a second line termination to actually send the command. A single write will
+    # thus not yield anything, e.g. calling 'timestamp.mode' will attempt to execute 'int("")'
+    # that fails with ValueError. Using this test, we patch extra enter commands.
+    try:
+        timestamp.mode  # required!
+    except ValueError:
+
+        def write(self, message: bytes, *args, **kwargs):
+            message = b";;".join(message.split(b";"))
+            message = b"\n\r\n".join(message.split(b"\n"))
+            self._write(message, *args, **kwargs)
+
+        timestamp._com._write = timestamp._com.write
+        timestamp._com.write = types.MethodType(write, timestamp._com)
+        timestamp._com.write(b"abort\r\n")  # terminate any existing streams
+        timestamp._com.readlines()  # drain buffer
+
     return timestamp
 
 
